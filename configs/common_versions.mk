cb56701ea31886ad9c920389fab48b1d2516e48f
Francis Rosario <francisrosario@ymail.com>
2012-06-01 06:08:48 +0300

    OTA
    Added Some Changes On The New OTA App.

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,14 +1,10 @@
-# Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
-
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=teamkang \
-    ro.aokp.version=$(TARGET_PRODUCT)_build-37
+    ro.rommanager.developerid=ChaosFirZen \
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.goo.developerid=aokp \
-    ro.goo.rom=aokp \
-    ro.goo.version=15
+    ro.goo.developerid=ChaosFirZen \
+    ro.goo.rom=MooDeDICS \
+    ro.goo.version=1
 
